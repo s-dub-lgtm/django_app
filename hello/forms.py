f0f41a5102ca84198c613f6c1924e6cefe2702e8
s-dub <ckl15076@nitech.jp>
2020-02-10 01:41:40 +0900

    complete ch3-4

--- hello/forms.py
+++ hello/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from .models import Friend
 
 
-class HelloForm(forms.Form):
-    id = forms.IntegerField(label='ID')
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name', 'mail', 'gender', 'age', 'birthday']
