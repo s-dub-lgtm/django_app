164affbc86ff59c3c961d55987b608dc3a7b4d34
s-dub <ckl15076@nitech.jp>
2020-02-10 00:27:13 +0900

    complete ch3-3

--- hello/views.py
+++ hello/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from .models import Friend
+from django.db.models import QuerySet
 
 
-class HelloView(TemplateView):
-    def __init__(self):
-        self.params = {
-            'title': 'Hello',
-            'form': HelloForm(),
-            'result': None
-        }
+def __new_str__(self):
+    result = ''
+    for item in self:
+        result += '<tr>'
+        for k in item:
+            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+        result += '</tr>'
+    return result
 
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
 
-    def post(self, request):
-        ch = request.POST.getlist('choice')
-        result = '<ol><b>selected:</b>'
-        for item in ch:
-            result += '<li>' + item + '</li>'
-        result += '</ol>'
-        self.params['result'] = result
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
+QuerySet.__str__ == __new_str__
+
+
+def index(request):
+    data = Friend.objects.all().values('id', 'name', 'age')
+    params = {
+        'title': 'Hello',
+        'data': data,
+    }
+    return render(request, 'hello/index.html', params)
